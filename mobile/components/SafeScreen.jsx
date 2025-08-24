4da37ec692d4c21c966ea425dddef278183fd4c5
Ramos Roiman <ramosroiman@gmail.com>
2025-08-24 17:32:00 -0400

    images, styles, authentication features using clerk

---
+++ mobile/components/SafeScreen.jsx
@@ -0,0 +1,14 @@
+import { Text, View } from 'react-native'
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { COLORS } from "@/constants/colors.js"
+
+const SafeScreen = ({ children }) => {
+    const insets = useSafeAreaInsets();
+    return (
+      <View style={{ paddingTop: insets.top, paddingBottom: insets.bottom, flex: 1, backgroundColor: COLORS.background }}>
+        {children}
+      </View>
+    );
+};
+
+export default SafeScreen
